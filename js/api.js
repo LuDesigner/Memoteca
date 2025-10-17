218e466e0d86410b04e067e630dff09913aacb7f
LuDesigner <luis.designer.oficial@gmail.com>
2025-10-17 17:31:42 -0300

    Atualizando projeto

--- js/api.js
+++ js/api.js
@@ -22,8 +22,48 @@ const api = {
             return await response.json()
         }
         catch {
-            alert('Erro ao buscar pensamentos')
+            alert('Erro ao salvar pensamentos')
             throw error
+        }
+    },
+
+    async buscarPensamentoPorId(id) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+            return await response.json()
+        }
+        catch {
+            alert('Erro ao buscar id pensamento')
+            throw error
+        }
+    },
+
+    async editarPensamento(pensamento) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: "PUT", 
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        }
+        catch {
+            alert('Erro ao editar pensamento')
+            throw error
+        }
+    },
+
+    async excluirPensamento(id) {
+        try {
+        const response = await fetch(`http://localhost:3000/pensamentos/${id}`, {
+            method: "DELETE"
+        })
+        }
+        catch {
+        alert('Erro ao excluir um pensamento')
+        throw error
         }
     }
 
