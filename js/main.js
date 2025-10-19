866502f30af7539fb70b3f78e87a64ef2915d56c
LuDesigner <luis.designer.oficial@gmail.com>
2025-10-19 13:29:27 -0300

    Atualizando final do projeto

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const botaoCancelar = document.getElementById("botao-cancelar")
   const inputBusca = document.getElementById("campo-busca")
 
-
   formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
   botaoCancelar.addEventListener("click", manipularCancelamento)
   inputBusca.addEventListener("input", manipularBusca)
@@ -19,12 +18,18 @@ async function manipularSubmissaoFormulario(event) {
   const id = document.getElementById("pensamento-id").value
   const conteudo = document.getElementById("pensamento-conteudo").value
   const autoria = document.getElementById("pensamento-autoria").value
+  const data = document.getElementById("pensamento-data").value
 
+  if (!validarData(data)) {
+    alert("Não é permitido o cadastro de datas futuras. Selecione outra data")
+    return
+  }
+  
   try {
     if (id) {
-      await api.editarPensamento({ id, conteudo, autoria })
+      await api.editarPensamento({ id, conteudo, autoria, data })
     } else {
-      await api.salvarPensamento({ conteudo, autoria })
+      await api.salvarPensamento({ conteudo, autoria, data })
     }
     ui.renderizarPensamentos()
   } catch {
@@ -40,11 +45,14 @@ async function manipularBusca() {
   const termoBusca = document.getElementById("campo-busca").value
   try {
     const pensamentosFiltrados = await api.buscarPensamentosPorTermo(termoBusca)
-    console.log(pensamentosFiltrados)
     ui.renderizarPensamentos(pensamentosFiltrados)
-  } catch (erro) {
+  } catch (error) {
     alert("Erro ao realizar busca")
-
   }
+}
 
+function validarData(data) {
+  const dataAtual = new Date()
+  const dataInserida = new Date(data)
+  return dataInserida <= dataAtual
 }
